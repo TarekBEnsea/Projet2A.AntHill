995b15de55afda2cb806219d53ceceb11675b6c2
Paul_Recamier <paul.recamier@ensea.fr>
2021-11-09 15:57:11 +0100

    Detection Collision

--- src/packRobot/Element.java
+++ src/packRobot/Element.java
@@ -4,11 +4,11 @@ public abstract class Element{
 	protected double posX; //horizontal, to the right
 	protected double posY; // vertical, downward
 	protected double rayon;
-	
+
 	public double getPosX(){return this.posX;}
 	public double getPosY(){return this.posY;}
 	
-	double getDistance(Element p){
+	public double getDistance(Element p){
 		return Math.hypot((this.posX-p.posX),(this.posY-p.posY));
 	}
 	public boolean enContact(Element r2){
