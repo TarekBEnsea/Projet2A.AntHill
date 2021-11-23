6e72cc39201a78a0592948ecb0c3c89b07a44d2e
bache <tarek.bache@ensea.fr>
2021-11-23 17:07:50 +0100

    commit

--- src/testxml/main.java
+++ src/testxml/main.java
@@ -1,23 +1,5 @@
 package testxml;
 
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.File;
-import java.io.IOException;
-
 public class main {
     public static void main(String[] args) {
 
