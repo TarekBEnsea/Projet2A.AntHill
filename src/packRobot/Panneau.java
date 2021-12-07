751be87557636c83d7f56e620ec50ec0bb8f7566
m0x0799 <94001698+m0x0799@users.noreply.github.com>
2021-12-07 15:47:32 +0100

    Update Panneau.java

--- src/packRobot/Panneau.java
+++ src/packRobot/Panneau.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import javax.swing.*;
 
 public class Panneau extends JPanel implements KeyListener {
 	private int posX = -50;
