9cf9c8b617ed5f2442df457b8246657093b8d797
m0x0799 <94001698+m0x0799@users.noreply.github.com>
2021-12-07 15:04:46 +0100

    Collision premerge

--- src/packRobot/Panneau.java
+++ src/packRobot/Panneau.java
@@ -1,10 +1,12 @@
 package packRobot;
 
+import testxml.InterXml;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
-import javax.swing.JPanel;
 
-public class Panneau extends JPanel {
+public class Panneau extends JPanel implements KeyListener {
 	private int posX = -50;
 	private int posY = -50;
 	private int theta = 0;
@@ -12,19 +14,21 @@ public class Panneau extends JPanel {
 	public ArrayList<Ressources> ressources;
   
 	public Panneau() {
+		this.setFocusable(true);
+		this.addKeyListener(this);
 
 	  	robots = new ArrayList<>();
 	  	ressources= new ArrayList<>();
 		//robots.add(new Robot(30,30,0));
-		//robots.add(new Robot(150,30,0));
 		for(int i = 0; i<50; i++) {
 			robots.add(new Robot());
 		}
 		for (int j =0; j<15;j++){
 			String name;
-			if(Math.random() < 0.5) name = "fraise";
-			else name = "pdt";
-			name="pdt";
+			double p = Math.random();
+			if(p < 0.33) name = "fraise";
+			else if (p <0.66) name = "pdt";
+			else name = "fb";
 			ressources.add(new Ressources(name));
 		}
 	}
@@ -51,6 +55,7 @@ public class Panneau extends JPanel {
 			  	fourmi1.mouvInPanel();
 				fourmi1.resetPprocheDistance();
 				fourmi1.setOrdreVitesseLigne(fourmi1.vitesseLigneMax);
+				
 				if (!fourmi1.getBroken()) {
 					for (int j = 0; j < robots.size(); j++) {
 						if (i != j) {
@@ -67,15 +72,19 @@ public class Panneau extends JPanel {
 							}
 						}
 					}
+					
 					for (int j = 0; j < ressources.size(); j++) {
 						ressource1 = ressources.get(j);
 						if (fourmi1.estProche(ressource1)) {
-							//if (fourmi1.enContact(ressource1)) {
+							if (fourmi1.enContact(ressource1)) {
 							//	  fourmi1.breakWheel();
-							//}
-							//else {
+								if(ressource1.getTaille() > 5) ressource1.setTaille(ressource1.getTaille()-5);
+								//ressources.remove(ressources.size()-1);
+								//this.ressources.remove(ressources.size()-1);
+							}
+							else {
 							fourmi1.eviter(fourmi1.getDirection(ressource1));
-							//}
+							}
 						}
 					}
 				}
@@ -91,4 +100,112 @@ public class Panneau extends JPanel {
 
 		}
   	}
+	
+	public void testgo(){
+		Robot fourmi1, fourmi2;
+		Ressources ressource1;
+		long past = System.currentTimeMillis();
+		long duration = 0;
+
+		InterXml comportementsimple = new InterXml("src/testxml/ComportementSimple");
+		for(Robot robot : robots) {
+			for(String comportement : comportementsimple.ReturnXmlNode("Comportement")){
+				robot.getListeComportement().add(comportement);
+			}
+		}
+
+		for(Robot robot : robots) XMLtoJava(robot, robot.getK(), comportementsimple);
+
+	  	while(robots.get(0).getListeComportement().size() > robots.get(0).getK() ){
+		  	for (int i=0; i<robots.size(); i++){
+				fourmi1=robots.get(i);
+
+				switch (fourmi1.getListeComportement().get(fourmi1.getK())){
+					case "MouvXY":
+						if(fourmi1.AvanceXY()) {
+							fourmi1.setK(fourmi1.getK() + 1);
+							XMLtoJava(fourmi1, fourmi1.getK(), comportementsimple);
+						};
+						break;
+					case "GoToXY":
+						if(fourmi1.AvanceXY()) {
+							fourmi1.setK(fourmi1.getK() + 1);
+							XMLtoJava(fourmi1, fourmi1.getK(), comportementsimple);
+						};
+						break;
+					case "Stop":
+						fourmi1.setTime(fourmi1.getTime() - duration);
+						if(fourmi1.getTime() < 0) {
+							fourmi1.setK(fourmi1.getK() + 1);
+							XMLtoJava(fourmi1, fourmi1.getK(), comportementsimple);
+						}
+						
+						break;
+					default:
+						break;
+					}
+				
+				/*fourmi1.setTime(fourmi1.getTime() - duration);
+				if(fourmi1.getTime() < 0) {
+					fourmi1.setK(fourmi1.getK() + 1);
+					fourmi1.setTime(comportementsimple.ReadCompState(fourmi1.getListeComportement().get(fourmi1.getK()), "time"));
+				}*/
+
+				for (int j=i+1; j<robots.size(); j++){
+				  	fourmi2=robots.get(j);
+				  	if(fourmi1.enContact(fourmi2)){
+					  	fourmi1.breakWheel();
+					  	fourmi2.breakWheel();
+				  	}
+			  	}
+			  	for (int j=0; j<ressources.size(); j++){
+				  	ressource1=ressources.get(j);
+				  	if(fourmi1.enContact(ressource1)){
+					  	fourmi1.breakWheel();
+						ressources.remove(ressources.get(j));
+				  	}
+			  	}
+			}
+
+			this.repaint();
+
+			try {
+				Thread.sleep(25);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+
+			long now = System.currentTimeMillis();
+			duration = now - past;
+			past = now;
+		}
+  	}
+
+    public void XMLtoJava(Robot robot, int k, InterXml comportementsimple){
+		switch (robot.getListeComportement().get(robot.getK())){
+			case "MouvXY":
+				robot.setAvanceX(comportementsimple.ReadCompState(robot.getListeComportement().get(k), "x") + robot.getPosX());
+				robot.setAvanceY(comportementsimple.ReadCompState(robot.getListeComportement().get(k), "y") + robot.getPosY());
+				break;
+			case "GoToXY":
+				robot.setAvanceX(comportementsimple.ReadCompState(robot.getListeComportement().get(k), "x"));
+				robot.setAvanceY(comportementsimple.ReadCompState(robot.getListeComportement().get(k), "y"));
+				break;
+			case "Stop":
+				robot.setTime(comportementsimple.ReadCompState(robot.getListeComportement().get(k), "time"));
+			default:
+				break;
+		}
+	}
+
+  	public void keyTyped(KeyEvent e) {}
+	public void keyPressed(KeyEvent e) {}
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_ESCAPE){
+			System.exit(1);
+		}
+
+	}
+
 }
