9622d565849ea81bf8dab5fdae40a26c30a31b13
Paul_Recamier <paul.recamier@ensea.fr>
2022-01-11 16:39:24 +0100

    Comportement + Ressources

--- src/packRobot/Panneau.java
+++ src/packRobot/Panneau.java
@@ -6,7 +6,6 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
-import java.util.concurrent.SynchronousQueue;
 
 import javax.swing.*;
 
@@ -15,6 +14,9 @@ public class Panneau extends JPanel implements KeyListener {
 	private int posX = -50;
 	private int posY = -50;
 	private int theta = 0;
+	private int cpt_fr = 0;
+	private int cpt_fb=0;
+	private int cpt_pdt=0;
 	public ArrayList<Robot> robots;
 	public ArrayList<Ressources> ressources;
 
@@ -25,10 +27,10 @@ public class Panneau extends JPanel implements KeyListener {
 	  	ressources= new ArrayList<>();
 		//robots.add(new Robot(30,30,0));
 		//robots.add(new Robot(150,30,0));
-		for(int i = 0; i<30; i++) {
+		for(int i = 0; i<200; i++) {
 			robots.add(new Robot());
 		}
-		for (int j =0; j<0;j++){
+		for (int j =0; j<5;j++){
 			String name;
 			double p = Math.random();
 			if(p < 0.33) name = "fraise";
@@ -50,7 +52,8 @@ public class Panneau extends JPanel implements KeyListener {
 		  	unress.draw(g);
 	  	}
 	}
-  
+
+	/*
   	public void go(){
 		Robot fourmi1, fourmi2;
 		Ressources ressource1;
@@ -87,9 +90,10 @@ public class Panneau extends JPanel implements KeyListener {
 
 		}
   	}
+  	*/
+
   	public void testgo(){
 		Robot fourmi1, fourmi2;
-		Ressources ressource1;
 		long past = System.currentTimeMillis();
 		long duration = 0;
 
@@ -108,29 +112,53 @@ public class Panneau extends JPanel implements KeyListener {
 				String oldName = fourmi1.getComportement().getName();
 				fourmi1.setComportement(new Comportement(fourmi1, robots, ressources, comportementsimple));
 				String newName = fourmi1.getComportement().getName();
+
+				for (Ressources ressource : ressources) {
+					if (fourmi1.enContact(ressource) && !fourmi1.isCarry()) {
+						if (ressource.getTaille() > 10) {
+							ressource.setTaille(ressource.getTaille() - 10);
+							fourmi1.setCarry(true);
+							switch (ressource.getName()) {
+								case "fraise" -> {
+									fourmi1.setImage("src/packRobot/ant+fr.png");
+									cpt_fr++;
+									System.out.println("Le nombre de fraise récuperée est de : " + cpt_fr);
+								}
+								case "fb" -> {
+									fourmi1.setImage("src/packRobot/ant+fb.png");
+									cpt_fb++;
+									System.out.println("Le nombre de framboise récuperée est de : " + cpt_fb);
+								}
+								case "pdt" -> {
+									fourmi1.setImage("src/packRobot/ant+pdt.png");
+									cpt_pdt++;
+									System.out.println("Le nombre de pomme de terre récuperée est de : " + cpt_pdt);
+								}
+								default -> {
+								}
+							}
+						}
+					}
+				}
+
 				if(!oldName.equals(newName)){
 					fourmi1.getComportement().XMLtoJava();
 					System.out.println("changement");
 				}
-				switch (newName){
-					case "MouvXY":
-						if(fourmi1.AvanceXY()) {
-							fourmi1.getComportement().XMLtoJava();
-						};
-						break;
-					case "GoToXY":
-						if(fourmi1.AvanceXY()) {
-							fourmi1.getComportement().XMLtoJava();
-						};
-						break;
-					case "Stop":
-						fourmi1.setTime(fourmi1.getTime() - duration);
-						if(fourmi1.getTime() < 0) {
+				switch (newName) {
+					case "MouvXY", "GoToXY" -> {
+						if (fourmi1.AvanceXY()) {
 							fourmi1.getComportement().XMLtoJava();
 						}
-						break;
-					default:
-						break;
+					}
+					case "Stop" -> {
+						fourmi1.setTime(fourmi1.getTime() - duration);
+						if (fourmi1.getTime() < 0) {
+							fourmi1.getComportement().XMLtoJava();
+						}
+					}
+					default -> {
+					}
 				}
 
 				/*fourmi1.setTime(fourmi1.getTime() - duration);
@@ -139,21 +167,6 @@ public class Panneau extends JPanel implements KeyListener {
 					fourmi1.setK(fourmi1.getK() + 1);
 					fourmi1.setTime(comportementsimple.ReadCompState(fourmi1.getListeComportement().get(fourmi1.getK()), "time"));
 				}*/
-
-				for (int j=i+1; j<robots.size(); j++){
-				  	fourmi2=robots.get(j);
-				  	if(fourmi1.enContact(fourmi2)){
-					  	fourmi1.breakWheel();
-					  	fourmi2.breakWheel();
-				  	}
-			  	}
-			  	for (int j=0; j<ressources.size(); j++){
-				  	ressource1=ressources.get(j);
-				  	if(fourmi1.enContact(ressource1)){
-					  	fourmi1.breakWheel();
-						ressources.remove(ressources.get(j));
-				  	}
-			  	}
 			}
 			this.repaint();
 
