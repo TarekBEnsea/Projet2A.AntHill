9283268b0c2862f958181a2f7e98c91ee24a53bb
m0x0799 <94001698+m0x0799@users.noreply.github.com>
2022-04-25 00:27:47 +0200

    javadoc

--- src/packRobot/MapBorder.java
+++ src/packRobot/MapBorder.java
@@ -1,8 +1,21 @@
 package packRobot;
 
+/**
+ * Bordure de la zone de simulation. Les Robots doivent évoluer entre 4 Bordure ayant chacune
+ * un des 4 type de tags de {@link BorderSide bordure}: {@link BorderSide#TOP TOP},{@link BorderSide#RIGHT RIGHT},{@link BorderSide#BOTTOM BOTTOM} ou {@link BorderSide#LEFT LEFT}
+ */
 public class MapBorder extends Element{
-    public final BorderSide side;
+    /** Type de Bordure*/
+    private final BorderSide side;
 
+    /**
+     * Crée une nouvelle MapBorder correspondant à un bord d'une zone rectangulaire.
+     * @param side un des 4 types de bordure possible {@link BorderSide#TOP TOP},{@link BorderSide#RIGHT RIGHT},
+     * {@link BorderSide#BOTTOM BOTTOM} ou {@link BorderSide#LEFT LEFT}
+     * @param position position sur l'axe orthogonal à l'axe de la bordure.
+     *                 position en X pour {@link BorderSide#TOP TOP} et {@link BorderSide#BOTTOM BOTTOM}
+     *                 position en Y pour {@link BorderSide#RIGHT RIGHT} et {@link BorderSide#LEFT LEFT}
+     */
     public MapBorder(BorderSide side, double position){
         this.side=side;
         switch (side){
@@ -15,6 +28,13 @@ public class MapBorder extends Element{
         }
     }
 
+    /**
+     * Renvoie la distance entre le centres du l'Element et cette bordure. Cette distance est définie par
+     * la plus proche distance entre le centre de l'Element et un point de la droite de la bordure soit un segment orthogonal
+     * à la bordure passant par l'Element.
+     * @param e Élément dont on veut la distance.
+     * @return distance à l'Element en argument
+     */
     @Override
     public double getDistance(Element e){
         switch (side){
@@ -25,41 +45,35 @@ public class MapBorder extends Element{
     }
 
     /**
-     * Renvoie la direction de la MapBorder par rapport à e.
+     * Renvoie la direction de l'Element e par rapport à la MapBorder.
      * @param e Element cible.
-     * @return Direction de la bordure.
+     * @return Direction de l'Element.
      */
     @Override
     public double getDirection(Element e){
         switch (side){
-            case TOP: return -Math.PI/2; //Yneg
-            case BOTTOM: return Math.PI/2; //Ypos
-            case LEFT: return Math.PI; //Xneg
-            case RIGHT: return 0; //xpos
+            case LEFT,RIGHT:
+                if(e.posX-posX>0) return 0; //Xpos
+                else return Math.PI; //Xneg
+            case TOP,BOTTOM:
+                if(e.posY-posY>0) return Math.PI/2; //Ypos
+                else return -Math.PI/2; //Yneg
             default: return 0; //erreur masquée
         }
     }
 
+    /**
+     * Renvoie la représentation en String de l'objet contenant le coté attribué ainsi que sa position.
+     * @return représentation en String de l'objet
+     */
     @Override
     public String toString() {
         return "MapBorder{" + side + "(" + (getPosX()) + ")}";
     }
-
-    public static void main(String[] args) {
-        Robot.initArea(400,200);
-        MapBorder coin1= new MapBorder(BorderSide.BOTTOM,Robot.getArea().getHeight());
-        MapBorder coin2= new MapBorder(BorderSide.TOP,0);
-        MapBorder coin3= new MapBorder(BorderSide.LEFT,0);
-        MapBorder coin4= new MapBorder(BorderSide.RIGHT,Robot.getArea().getWidth());
-        Robot roboto = new Robot(185,50,0,10);
-        roboto.resetPprocheDistance();
-        System.out.println(roboto.estProche(coin4, false));
-        System.out.println(roboto.getDistance(coin1));
-        System.out.println(roboto.getDistance(coin2));
-        System.out.println(roboto.getDistance(coin3));
-        System.out.println(roboto.getDistance(coin4));
-    }
 }
+/**
+ * Type de bordure. Représente un des côtés du rectangle que forme la zone de simulation.
+ */
 enum BorderSide{
     BOTTOM,TOP,LEFT,RIGHT
 }
