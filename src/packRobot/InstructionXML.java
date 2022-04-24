edbac6f3a48d2ec6e5f4734177da6c1805ed5318
m0x0799 <94001698+m0x0799@users.noreply.github.com>
2022-04-24 15:24:17 +0200

    javadoc (!!! manque 2 attributs à décrire)

--- src/packRobot/InstructionXML.java
+++ src/packRobot/InstructionXML.java
@@ -8,34 +8,56 @@ import java.awt.event.ItemListener;
 
 /**
  * Bloc instruction à instancier dans l'onglet de programmation simplifié.
- * Contient:
- *      ID+nom d'instruction
- *      tldw
+ * Chaque instance correspond à une fonction déclenchée selon certains capteurs et au dessous d'une
+ * certaine priorité
  */
 public class InstructionXML extends JPanel {
+    /** Nombre total d'instructions instanciées*/
     private static int nombreInstructions;
+    /** Nombre d'instruction dont il faut actualiser le numéro d'ID en cas de suppression*/
     private static int IDaUpdate=0;
+    /** Dernier numéro d'ID supprimé. Cet ID doit être remplacé par l'ID immédiatement supérieur sauf si
+     * il s'agissait du dernier*/
     private static int IDremoved;
+
+    /** Nom des triggers tels qu'affichés dans l'interface*/
     private final static String[] nomsTriggersAbrev = new String[]{"F proche","R proche","fin instruc","OOB", "has info", "F without info", "is carrying"};
+    /** Nom des triggers correspondants à leur balise XML*/
     private final static String[] nomsTriggersComplet = new String[]{"antsnextto","ressourcesnextto","lastcomportementfinished", "outofbound", "information", "fourmisansinfo", "iscarrying"};
     
+    /** Bouton de suppression d'instruction*/
     private JButton suppInstructionButton;
+    /** Label affichant le numéro d'ID*/
     private JLabel IDlabel;
+        /** numéro d'ID d'instruction. Celui-ci est unique parmis toutes les instructions encore instanciées*/
         private int instructionID;
+    /** Nom de l'instruction*/
     private JTextField nomInstruction;
+    /** Boite de sélection de la fonction à exécuter par l'instruction*/
     private JComboBox fonctionJ;
+        /** Nombre de paramètres à renseigner pour exécuter la fonction*/
         private int nombreParam;
-    private JTextField trigParam;
-    private int indexTrig;
+        /** nom des paramètres*/
         private JLabel[] nomFonctionParams;
+        /** valeur des paramètres*/
         private JTextField[] fonctionparams;
+    /** Panneau pour sélectionner les triggers (capteurs à valider pour exécuter l'instruction)*/
     private JPanel triggerPan = new JPanel();//"cases à cocher pour les triggers"
+        /** Tableau contenant les différentes boîtes de selection des triggers*/
         private JCheckBox[] triggers;
+        private JTextField trigParam;
+        private int indexTrig;
+    /** Champ pour renseigner la priorité, sous forme d'entiers naturels
+     * 0 est la priorité la plus forte, +inifity la priorité la plus faible*/
     private JTextField priorite;
-    private JComboBox typeLimite;
-        private JTextField limite;
+    /*private JComboBox typeLimite;
+        private JTextField limite;*/
 
-
+    /**
+     * Crée un nouveau panneau de remplissage d'une instruction.
+     * Les différents champs peuvent être remplis ou sélectionnés.
+     * Cette instruction peut aussi être supprimée.
+     */
     public InstructionXML() {
         instructionID=nombreInstructions++;
         suppInstructionButton = new JButton("-");
@@ -47,13 +69,15 @@ public class InstructionXML extends JPanel {
             triggers[i] = new JCheckBox(nomsTriggersAbrev[i]);
         }
         priorite = new JTextField(2);
-        typeLimite = new JComboBox<>(new String[]{"*Limite*","None","temps(ms)","distance(p)"});
-        limite = new JTextField(3);
+        /*typeLimite = new JComboBox<>(new String[]{"*Limite*","None","temps(ms)","distance(p)"});
+        limite = new JTextField(3);*/
 
         initFonction();
         insertInPane();
     }
 
+    /** Initialise les différentes actions exécutées lors de la sélection d'un item.
+     * Ces actions permettent de faire apparaître les champs à remplir lorsque c'est nécessaire.*/
     private void initFonction(){
         fonctionJ.addActionListener(new ActionListener() {
             @Override
@@ -81,7 +105,7 @@ public class InstructionXML extends JPanel {
             }
         });
 
-        typeLimite.addActionListener(new ActionListener() {
+        /*typeLimite.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String selectedItem=typeLimite.getSelectedItem().toString();
@@ -92,9 +116,10 @@ public class InstructionXML extends JPanel {
                 }
                 validate(); repaint();
             }
-        });
+        });*/
     }
 
+    /** Retire de l'affichage les paramètres de la fonction précédemment sélectionnée*/
     private  void removeFonctionParam(){for(int i=0; i<2*nombreParam;i++) remove(4);}
     private void affiFonctionParam(){
         for(int i=0; i<nombreParam;i++){
@@ -104,6 +129,7 @@ public class InstructionXML extends JPanel {
         validate(); repaint();
     }
 
+    /** Ajoute les différents composants de l'instruction au {@link JPanel panneau} principal*/
     private void insertInPane(){
         add(suppInstructionButton);
         add(IDlabel);
@@ -135,13 +161,20 @@ public class InstructionXML extends JPanel {
         add(triggerPan);
         add(new JLabel("Priorité:"));
         add(priorite);
-        add(typeLimite);
+        //add(typeLimite);
     }
 
+    /**
+     * Génère un tableau contenant les noms des paramètres de l'instruction ainsi que leur valeur.
+     * Chaque couple commence par le nom du paramètre, qui est le même que le nom de la balise XML, puis par la valeur.
+     * @return Tableau des paramètres
+     */
     public String[] generateSynthTab(){
         String[] synthTab = new String[6+2*nombreParam+2*nomsTriggersAbrev.length];
         synthTab[0]="name";
-        synthTab[1]=fonctionJ.getSelectedItem().toString();
+        try{
+            synthTab[1]=fonctionJ.getSelectedItem().toString();
+        } catch (NullPointerException e) {return null;}
         for(int i=0;i<nombreParam;i++){
             synthTab[2+2*i]=nomFonctionParams[i].getName();
             synthTab[3+2*i]=fonctionparams[i].getText();
@@ -162,10 +195,25 @@ public class InstructionXML extends JPanel {
         }
         return synthTab;
     }
+
+    /**
+     * Renvoie le nombre d'instructions actuellement instanciées.
+     * @return nombre d'instructions instanciées
+     */
     public static int getNombreInstructions() {return nombreInstructions;}
     //public int getInstructionID() {return instructionID;}
+
+    /**
+     * Renvoie le bouton de suppression d'instruction afin de pouvoir récupérer la fonction exécutée par son click.
+     * @return bouton de suppression d'instruction.
+     */
     public JButton getSuppInstructionButton() {return suppInstructionButton;}
     //private class IdOverridenException extends Exception{}
+
+    /**
+     * Diminue lorsque cela est autorisé l'ID de l'instruction.
+     * Cette fonction doit être appelée après la suppression d'une instruction.
+     */
     public void decrID(){
         if(IDaUpdate>0 && instructionID>IDremoved){
             instructionID--;
@@ -174,6 +222,11 @@ public class InstructionXML extends JPanel {
         }
         //else throw new IdOverridenException();
     }
+
+    /**
+     * Enregistre le nom de l'instruction supprimée afin d'actualiser les ID supérieurs.
+     * @return l'instruction supprimée
+     */
     public int delAndUpdate(){
         IDaUpdate=nombreInstructions-instructionID-1;
         nombreInstructions--;
